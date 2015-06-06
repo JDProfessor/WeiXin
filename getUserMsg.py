d9658e2072fd5b58fda742de876a64fc4723e8d5
JD <91xiaodian@sina.com>
2015-06-06 13:56:20 +0800

    add error handle

--- getUserMsg.py
+++ getUserMsg.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 #
-import tokenGenerator
+import tokenGenerator,sys
 
 def get_user_id():
 	access_token = tokenGenerator.getToken()
 	url = 'https://api.weixin.qq.com/cgi-bin/user/get?access_token=%s'%access_token
 	response_msg = eval(tokenGenerator.request_get(url))
-	return response_msg["data"]["openid"] 
+	try:
+		return response_msg["data"]["openid"] 
+	except KeyError:
+		print response_msg
+		sys.exit(1)
 
 if __name__ == '__main__':
 	print get_user_id()	
